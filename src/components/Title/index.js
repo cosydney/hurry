f5396c360cd209b5c0769068fce93360aa6eb3f7
Sydney <chnsydney@gmail.com>
2019-06-21 09:21:08 +0200

    mobile background header

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -2,13 +2,14 @@ import React from 'react'
 import Confetti from "../../images/confetti-bg.png";
 
 const Title = () => {
+  const isMobile = window.innerWidth <= 500;
   return (
     <div style={{display: 'flex', justifyContent: 'space-around', flexDirection: 'column', margin: '20px 0px 0px 0px', minHeight: 413}}>
       <h1>Harry</h1>
       <h1 style={{zIndex: 2, fontSize: 42, color: '#232E50',  fontWeight: 600}}>The best way to increase the attendance
 rate at your Eventbrite events</h1>
       <h2 style={{zIndex: 2, color: '#232E50'}}>Simply import you event contacts, schedule  your messages and send them !</h2>
-      <div className='confetti' style={{background: `url(${Confetti})`}}></div>
+      <div className='confetti' style={{background: `url(${Confetti})`, right: isMobile ? 0 : 144}}></div>
     </div>
   )
 }
