021f029ebc40a47edf95030e7efafe92d542066c
Sydney <chnsydney@gmail.com>
2019-06-29 10:38:30 +0200

    "production"

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -1,8 +1,8 @@
 // TODO
-export const URL = (process.env.REACT_APP_NODE === 'development' ?
+export const URL = (process.env.REACT_APP_NODE === 'production' ?
   'https://harryapi.herokuapp.com/'
   : 'http://localhost:1337/');
 
-export const URLFront = (process.env.REACT_APP_NODE === 'development' ?
+export const URLFront = (process.env.REACT_APP_NODE === 'production' ?
 'https://www.thankyouharry.co/'
 : 'http://localhost:3000/');
