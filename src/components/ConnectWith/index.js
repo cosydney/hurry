9db3bf01793c2220fdb25266b8b8a20d06847ae9
Sydney <chnsydney@gmail.com>
2019-07-01 17:19:55 +0200

    "venue"

--- src/components/ConnectWith/index.js
+++ src/components/ConnectWith/index.js
@@ -220,7 +220,7 @@ class ConnectWith extends Component {
                             </div>
                             <div>
                               <h4>{formatAMPM(new Date(event.start.utc))}</h4>
-                              {event.venue &&
+                              {(event.venue && event.venue.address.localized_address_display) &&
                                 <h4>{event.venue.name}</h4>
                                 <h4>{event.venue.address.localized_address_display}</h4>
                               }
