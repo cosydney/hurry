71bb456d4d485ad85f9ea6fd4b02ca334d833e45
Sydney <chnsydney@gmail.com>
2019-07-08 15:51:53 +0200

    "stripe keys"

--- src/utils/stripe.js
+++ src/utils/stripe.js
@@ -1,3 +1,3 @@
 export const STRIPE_PUBLIC_KEY = (process.env.REACT_APP_NODE === 'production' ?
-  'pk_test_pidSD5oyADBSxIq7cU7Q0ZQI00R319YN5P'
+  'pk_live_ZY3s4GVE3qj2sJtLFEmrD9ji00NH0ycMoL'
   : 'pk_test_pidSD5oyADBSxIq7cU7Q0ZQI00R319YN5P');
