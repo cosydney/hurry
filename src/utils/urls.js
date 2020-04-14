86e66f881243cabf967ce709f81bcdd43cb20278
Sydney <chnsydney@gmail.com>
2020-04-14 09:58:03 +0200

    "production url"

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -1,6 +1,6 @@
 // TODO
 export const URL = (process.env.REACT_APP_NODE === 'production' ?
-  'https://api.thankyouharry.co/'
+  'https://thankyouharry.herokuapp.com/'
   : 'http://localhost:1337/');
 
 export const URLFront = (process.env.REACT_APP_NODE === 'production' ?
